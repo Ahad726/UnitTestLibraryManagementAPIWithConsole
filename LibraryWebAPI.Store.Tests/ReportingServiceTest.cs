4f1b878c60056fbe873751b9a2ed3a7229e5f274
Ahad726 <atmahad726@gmail.com>
2019-10-19 19:17:06 +0600

    Reporting Service calculate method test.

--- LibraryWebAPI.Store.Tests/ReportingServiceTest.cs
+++ LibraryWebAPI.Store.Tests/ReportingServiceTest.cs
@@ -4,6 +4,7 @@ using LibraryWebAPI.Store.IRepositories;
 using LibraryWebAPI.Store.IServices;
 using LibraryWebAPI.Store.Services;
 using NUnit.Framework;
+using Shouldly;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
@@ -21,7 +22,7 @@ namespace LibraryWebAPI.Store.Tests
         [OneTimeSetUp]
         public void ClassSetUp()
         {
-            _mock = AutoMock.GetLoose();
+            
         }
 
 
@@ -29,19 +30,19 @@ namespace LibraryWebAPI.Store.Tests
         public void ClassCleanUp()
         {
 
-            _mock?.Dispose();
+           
         }
 
         [SetUp]
         public void TestSetUp()
         {
-
+            _mock = AutoMock.GetLoose();
         }
 
         [TearDown]
         public void TestCleanUp()
         {
-
+            _mock?.Dispose();
         }
 
         [Test]
@@ -78,6 +79,44 @@ namespace LibraryWebAPI.Store.Tests
 
 
         }
+
+
+        [Test]
+        public void CalculateFine_WhenCalledWithStudentId_ReturnStudentFine()
+        {
+
+            // Arrange
+            int studentId = 1618017;
+            DateTime issueDate = Convert.ToDateTime("10/19/2019");
+            DateTime returnDate = Convert.ToDateTime("10/29/2019");
+            double Fine = 20.00;
+
+            var bookIssueRepositoryMock = _mock.Mock<IBookIssueRepository>();
+            var returnBookRepositoryMock = _mock.Mock<IReturnBookRepository>();
+            var studentRepositoryMock = _mock.Mock<IStudentRepository>();
+            var unitOfWorkLibraryService = _mock.Mock<IUnitOfWorkLibraryService>();
+
+            bookIssueRepositoryMock.Setup(x => x.GetBookIssueDate(studentId)).Returns(issueDate);
+            returnBookRepositoryMock.Setup(x => x.GetBookReturnDate(studentId)).Returns(returnDate);
+            studentRepositoryMock.Setup(x => x.SetStudentFine(studentId, Fine));
+
+            unitOfWorkLibraryService.Setup(x => x.BookIssueRepositor).Returns(bookIssueRepositoryMock.Object);
+            unitOfWorkLibraryService.Setup(x => x.ReturnBookRepository).Returns(returnBookRepositoryMock.Object);
+            unitOfWorkLibraryService.Setup(x => x.StudentRepository).Returns(studentRepositoryMock.Object);
+
+            _reportingService = _mock.Create<ReportingService>
+                (new TypedParameter(typeof(UnitOfWorkLibraryService), unitOfWorkLibraryService.Object));
+
+            // Act
+            var actualFine =  _reportingService.CalculateFine(studentId);
+
+
+
+            // Assert
+            actualFine.ShouldBe(Fine);
+
+        }
+
 
 
 
